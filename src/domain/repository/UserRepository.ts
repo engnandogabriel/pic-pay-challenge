17a75d01b89adcec18e9f73750791cab71f0b56c
Nando Gabriel <eng.nandogabriel@gmail.com>
2024-04-13 18:35:39 -0300

    fix: fix bug in UserRepository.ts

--- src/domain/repository/UserRepository.ts
+++ src/domain/repository/UserRepository.ts
@@ -3,5 +3,5 @@ import User from "../entities/User";
 export default interface UserRepository {
   save(data: User): Promise<void>;
   getUserByDocument(document: string): Promise<User | void>;
-  updateAmount(data: User): Promise<User | void>;
+  updateAmount(data: User): Promise<void>;
 }
