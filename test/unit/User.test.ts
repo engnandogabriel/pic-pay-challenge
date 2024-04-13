2c33282f115ed77896c556f1a5b2c9859f7b052b
Nando Gabriel <eng.nandogabriel@gmail.com>
2024-04-13 15:14:24 -0300

    refactor adding factory desfing patter in User.test.ts

--- test/unit/User.test.ts
+++ test/unit/User.test.ts
@@ -1,27 +1,28 @@
 import User from "../../src/domain/entities/User";
 
 test("Should be created a User valid", () => {
-  const user = new User(
+  const user = User.create(
     "John Doe",
     "12345678910",
     "johndoe@gmail.com",
     "password123",
     "commun"
   );
-  expect(user.name).toBe("John Doe");
-  expect(user.document.getValue()).toBe("123.456.789-10");
-  expect(user.email.getValue()).toBe("johndoe@gmail.com");
-  expect(user.password).toBe("password123");
-  expect(user.type).toBe("commun");
+  expect(user.getId()).toBeDefined();
+  expect(user.getName()).toBe("John Doe");
+  expect(user.getDocument()).toBe("123.456.789-10");
+  expect(user.getEmail()).toBe("johndoe@gmail.com");
+  expect(user.getPassword()).toBe("password123");
+  expect(user.getTypeUser()).toBe("commun");
 });
 
 test("Should be create a User with a CNPJ valid", () => {
-  const user = new User(
+  const user = User.create(
     "John Doe",
     "12345678000199",
     "johndoe@gmail.com",
     "password123",
     "commun"
   );
-  expect(user.document.getValue()).toBe("12.345.678/0001-99");
+  expect(user.getDocument()).toBe("12.345.678/0001-99");
 });
