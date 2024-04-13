f76296fec31c123fea2f935b72264678dabc6a8a
Nando Gabriel <eng.nandogabriel@gmail.com>
2024-04-13 16:14:42 -0300

    refactor: test added to amount in User

--- test/unit/User.test.ts
+++ test/unit/User.test.ts
@@ -6,7 +6,8 @@ test("Should be created a User valid", async () => {
     "12345678910",
     "johndoe@gmail.com",
     "password123",
-    "commun"
+    "commun",
+    1000
   );
   expect(user.getId()).toBeDefined();
   expect(user.getName()).toBe("John Doe");
@@ -14,6 +15,7 @@ test("Should be created a User valid", async () => {
   expect(user.getEmail()).toBe("johndoe@gmail.com");
   expect(user.getPassword()).toBeDefined();
   expect(user.getTypeUser()).toBe("commun");
+  expect(user.getAmount()).toBe(1000);
 });
 
 test("Should be create a User with a CNPJ valid", async () => {
@@ -22,7 +24,8 @@ test("Should be create a User with a CNPJ valid", async () => {
     "12345678000199",
     "johndoe@gmail.com",
     "password123",
-    "commun"
+    "commun",
+    1000
   );
   expect(user.getDocument()).toBe("12.345.678/0001-99");
 });
@@ -34,8 +37,24 @@ test("Should verify if password is valid", async () => {
     "12345678910",
     "johndoe@gmail.com",
     "594520296218431dbbf864b971f3d5fb6b45dce35038bd66df4f279db23937d97672f30e9a63f1f6c5be5cc787d0f38d26021a0497e5e458c4483d757e236cec",
-    "commun"
+    "commun",
+    1000
   );
   const validate = await user.validatePassword("password123");
   expect(validate).toBe(true);
+});
+
+test("Shold be added 500 and disconted 1000 in amount of the User", async () => {
+  const user = await User.create(
+    "John Doe",
+    "12345678000199",
+    "johndoe@gmail.com",
+    "password123",
+    "commun",
+    1000
+  );
+  user.add(500);
+  expect(user.getAmount()).toBe(1500);
+  user.discont(1000);
+  expect(user.getAmount()).toBe(500);
 });
