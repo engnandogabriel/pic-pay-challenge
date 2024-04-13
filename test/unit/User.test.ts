7f5c7f88439fb8c12d34098e44a6e20d2aa2c274
Nando Gabriel <eng.nandogabriel@gmail.com>
2024-04-13 13:33:58 -0300

    test: add test to validate User class

---
+++ test/unit/User.test.ts
@@ -0,0 +1,14 @@
+test("Should be created a User", () => {
+  const user = new User(
+    "John Doe",
+    "123456789-10",
+    "johndoe@gmail.com",
+    "password123",
+    "commun"
+  );
+  expect(user.name).toBe("John Doe");
+  expect(user.document).toBe("123456789-10");
+  expect(user.email).toBe("johndoe@gmail.com");
+  expect(user.password).toBe("password123");
+  expect(user.type).toBe("commun");
+});
