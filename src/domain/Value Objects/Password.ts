8a5fbf450c4961f1bed752ce54d3399fa97e5df2
Nando Gabriel <eng.nandogabriel@gmail.com>
2024-04-13 15:14:49 -0300

    feat: adding Value Object Passwod

---
+++ src/domain/Value Objects/Password.ts
@@ -0,0 +1,35 @@
+import { pbkdf2 } from "crypto";
+export default class Password {
+  private value: string;
+  constructor(password: string) {
+    this.value = password;
+  }
+
+  static create(password: string): Promise<Password> {
+    if (password.length < 8) throw new Error("Invalid password");
+    const generatedSalt = "saltpassword";
+    return new Promise((resolve) => {
+      pbkdf2(password, generatedSalt, 100, 64, "sha512", (error, value) => {
+        resolve(new Password(value.toString("hex")));
+      });
+    });
+  }
+  async validate(plainPassword: string) {
+    return new Promise((resolve) => {
+      pbkdf2(
+        plainPassword,
+        "saltpassword",
+        100,
+        64,
+        "sha512",
+        (error, value) => {
+          resolve(this.value === value.toString("hex"));
+        }
+      );
+    });
+  }
+
+  getValue() {
+    return this.value;
+  }
+}
