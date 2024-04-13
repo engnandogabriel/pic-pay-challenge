aa2bc885fe01cd53e2f399e493a8bd559b613a96
Nando Gabriel <eng.nandogabriel@gmail.com>
2024-04-13 18:34:57 -0300

    feat: added CreateUserUseCase.ts

---
+++ src/application/CreateUserUseCase.ts
@@ -0,0 +1,31 @@
+import HttpRequest from "../domain/Protocols/HttpRequest";
+import HttpResponse from "../domain/Protocols/HttpResponse";
+import User from "../domain/entities/User";
+import UserRepository from "../domain/repository/UserRepository";
+import UseCase from "./UseCase";
+
+export default class CreateUserUseCase implements UseCase {
+  private userRepository: UserRepository;
+  constructor(userRepository: UserRepository) {
+    this.userRepository = userRepository;
+  }
+  async execute(data: HttpRequest): Promise<HttpResponse> {
+    try {
+      const user = await User.create(
+        data.body.name,
+        data.body.document,
+        data.body.email,
+        data.body.password,
+        data.body.type,
+        data.body.amount
+      );
+      await this.userRepository.save(user);
+      return { statusCode: 201, body: "User created" };
+    } catch (error) {
+      if (error instanceof Error) {
+        return { statusCode: 422, body: "User do cnot reated" };
+      }
+      return { statusCode: 500, body: "Unexpected Error" };
+    }
+  }
+}
