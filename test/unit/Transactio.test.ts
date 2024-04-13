042221fbf4fad6f20fc8e42ff6a3a46b8f605185
Nando Gabriel <eng.nandogabriel@gmail.com>
2024-04-13 16:35:55 -0300

    refactory: add test in Transaction.test.ts

--- test/unit/Transactio.test.ts
+++ test/unit/Transactio.test.ts
@@ -1,7 +1,26 @@
-test("Shold be create a new Transaction", () => {
-  const transaction = new Transaction("1", "2", 600);
-  expect(transaction.id).toBeDefined();
-  expect(transaction.payer).toBe("1");
-  expect(transaction.payee).toBe("2");
-  expect(transaction.value).toBe(600);
+import Transaction from "../../src/domain/entities/Transaction";
+import User from "../../src/domain/entities/User";
+
+test("Shold be create a new Transaction", async () => {
+  const payer = await User.create(
+    "johndoe",
+    "12345678910",
+    "johndoe@gmail.com",
+    "password123",
+    "commun",
+    1000
+  );
+  const payee = await User.create(
+    "nando",
+    "45678912345",
+    "nando@gmail.com",
+    "password456",
+    "merchant",
+    30
+  );
+  const transaction = Transaction.create(payer, payee, 600);
+  expect(transaction.getId()).toBeDefined();
+  expect(transaction.getPayer()).toBeDefined();
+  expect(transaction.getPayee()).toBeDefined();
+  expect(transaction.getValue()).toBe(600);
 });
