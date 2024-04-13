80c32c019c4657356e97ac71267e58a0745d4a66
Nando Gabriel <eng.nandogabriel@gmail.com>
2024-04-13 15:35:47 -0300

    refactor: add validate pass in  User.test.ts

--- test/unit/User.test.ts
+++ test/unit/User.test.ts
@@ -1,7 +1,7 @@
 import User from "../../src/domain/entities/User";
 
-test("Should be created a User valid", () => {
-  const user = User.create(
+test("Should be created a User valid", async () => {
+  const user = await User.create(
     "John Doe",
     "12345678910",
     "johndoe@gmail.com",
@@ -12,12 +12,12 @@ test("Should be created a User valid", () => {
   expect(user.getName()).toBe("John Doe");
   expect(user.getDocument()).toBe("123.456.789-10");
   expect(user.getEmail()).toBe("johndoe@gmail.com");
-  expect(user.getPassword()).toBe("password123");
+  expect(user.getPassword()).toBeDefined();
   expect(user.getTypeUser()).toBe("commun");
 });
 
-test("Should be create a User with a CNPJ valid", () => {
-  const user = User.create(
+test("Should be create a User with a CNPJ valid", async () => {
+  const user = await User.create(
     "John Doe",
     "12345678000199",
     "johndoe@gmail.com",
@@ -25,4 +25,17 @@ test("Should be create a User with a CNPJ valid", () => {
     "commun"
   );
   expect(user.getDocument()).toBe("12.345.678/0001-99");
+});
+
+test("Should verify if password is valid", async () => {
+  const user = User.restore(
+    "123",
+    "johndoe@gmail.com",
+    "12345678910",
+    "johndoe@gmail.com",
+    "594520296218431dbbf864b971f3d5fb6b45dce35038bd66df4f279db23937d97672f30e9a63f1f6c5be5cc787d0f38d26021a0497e5e458c4483d757e236cec",
+    "commun"
+  );
+  const validate = await user.validatePassword("password123");
+  expect(validate).toBe(true);
 });
