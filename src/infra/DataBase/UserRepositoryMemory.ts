a23f18255cf2d66893f109265893ed1bda932858
Nando Gabriel <eng.nandogabriel@gmail.com>
2024-04-13 18:36:14 -0300

    feat: added UserRepositoryMemory implementation

---
+++ src/infra/DataBase/UserRepositoryMemory.ts
@@ -0,0 +1,24 @@
+import User from "../../domain/entities/User";
+import UserRepository from "../../domain/repository/UserRepository";
+
+export default class UserRepositoryMemor implements UserRepository {
+  user: Array<User>;
+  constructor() {
+    this.user = [];
+  }
+  async save(data: User): Promise<void> {
+    this.user.push(data);
+  }
+  async getUserByDocument(document: string): Promise<void | User> {
+    for (const e of this.user) {
+      if (e.getDocument() === document) return e;
+    }
+  }
+  async updateAmount(data: User): Promise<void> {
+    for (const e of this.user) {
+      if (e.getDocument() === data.getDocument()) {
+        e.getAmount();
+      }
+    }
+  }
+}
