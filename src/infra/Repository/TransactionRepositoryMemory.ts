dba6983c5cfef6ec3eb930713076702e15f8100a
Nando Gabriel <eng.nandogabriel@gmail.com>
2024-04-13 22:02:37 -0300

    feat: add TransacionRepositoryMemory.ts

---
+++ src/infra/Repository/TransactionRepositoryMemory.ts
@@ -0,0 +1,19 @@
+import Transaction from "../../domain/entities/Transaction";
+import TransactionRepository from "../../domain/repository/TransactionRepository";
+
+export default class TransactionRepositoryMemory
+  implements TransactionRepository
+{
+  transaction: Array<Transaction>;
+  constructor() {
+    this.transaction = [];
+  }
+  async save(data: Transaction): Promise<void> {
+    this.transaction.push(data);
+  }
+  async getTransactionId(id: string): Promise<void | Transaction> {
+    for (const transaction of this.transaction) {
+      if (transaction.getId() === id) return transaction;
+    }
+  }
+}
